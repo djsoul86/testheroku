06c1d742ecf934502cce58c2cce13193f243bc52
djsoul86 <ernestoandrade86@gmail.com>
2018-07-05 10:00:22 -0500

    Se ajusta modelo usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,7 +1,6 @@
 class Usuario < ApplicationRecord
     validates :nombre, presence: true
     validates :edad, presence: true
-    validates :password, presence: true
     validates :apellido, presence: true
 end
 
