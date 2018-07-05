4f34e13746826dc5abcc277769e28c54c4c57700
djsoul86 <ernestoandrade86@gmail.com>
2018-07-05 10:22:15 -0500

    Se agregan propiedades al objeto usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,6 +1,10 @@
 class Usuario < ApplicationRecord
     validates :nombre, presence: true
-    validates :edad, presence: true
     validates :apellido, presence: true
+    validates :email, presence: true
+    validates :password, presence: true
+    validates :documento, presence: true
+    validates :celular, presence: true
+    validates :direccion, presence: true
 end
 
